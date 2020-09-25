894dff67c112a077d308a4cf7155f78801917567
jay <johnjohnsonokah@gmail.com>
2020-09-25 15:28:16 +0100

    Change database url

--- disposify/settings.py
+++ disposify/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "disposify.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+DATABASES = {"default": dj_database_url.config(default=config("sqlite:///db.sqlite3"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
