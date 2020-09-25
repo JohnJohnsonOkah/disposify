2ba810721327a84f48dda9e9ecce6d7b6c7e61c0
jay <johnjohnsonokah@gmail.com>
2020-09-25 15:50:25 +0100

    Use db_from_env

--- disposify/settings.py
+++ disposify/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = "disposify.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASE_URL = config("DATABASE_URL")
-DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
